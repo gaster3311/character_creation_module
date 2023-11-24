46fd34a893df76b226c71ac5132a864e4d3b9b27
gaster3311 <daniel.samoshin@gmail.com>
2023-11-24 13:35:50 +0300

    added docstrings to main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """
+    Calculate damage during an attack based on character class.
+
+    Args:
+        char_name (str): The character's name.
+        char_class (str): Character class ('warrior', 'mage', or 'healer').
+
+    Returns:
+        str: A string indicating the damage dealt by the character.
+    """
     damage: int = 5
     if char_class == 'warrior':
         damage += randint(3, 5)
@@ -13,6 +25,16 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """
+    Calculate block strength during defense based on character class.
+
+    Args:
+        char_name (str): The character's name.
+        char_class (str): Character class ('warrior', 'mage', or 'healer').
+
+    Returns:
+        str: A string indicating the block strength during defense.
+    """
     block: int = 10
     if char_class == 'warrior':
         block += randint(5, 10)
@@ -24,25 +46,41 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """
+    Use special abilities based on character class.
+
+    Args:
+        char_name (str): The character's name.
+        char_class (str): Character class ('warrior', 'mage', or 'healer').
+
+    Returns:
+        str: A string indicating the special ability used by the character.
+    """
     abilities: set = {
         'warrior': ('Выносливость', 80 + 25),
         'mage': ('Атака', 5 + 40),
-        'healer': ('Защита', 10 + 30)
+        'healer': ('Защита', 10 + 30),
     }
     ability_name, ability_power = abilities[char_class]
     return f'{char_name} применил «{ability_name} {ability_power}»'
 
 
 def start_training(char_name: str, char_class: str) -> str:
-    classes: set = {
-        'warrior': 'Воитель',
-        'mage': 'Маг',
-        'healer': 'Лекарь'
-    }
+    """
+    Begin character training with attack, defense, and special abilities.
+
+    Args:
+        char_name (str): The character's name.
+        char_class (str): Character class ('warrior', 'mage', or 'healer').
+
+    Returns:
+        str: A string indicating the end of the training.
+    """
+    classes: set = {'warrior': 'Воитель', 'mage': 'Маг', 'healer': 'Лекарь'}
     class_name = classes[char_class]
     print(f'{char_name}, ты {class_name} — отличный боец!')
     print('Тренируйся: attack, defence, special, skip')
-    cmd = None
+    cmd: str = None
     while cmd != 'skip':
         cmd = input('Команда: ')
         if cmd == 'attack':
@@ -54,9 +92,15 @@ def start_training(char_name: str, char_class: str) -> str:
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    """
+    Choose a character class for the player.
+
+    Returns:
+        str: The selected character class ('warrior', 'mage', or 'healer').
+    """
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, '
                            'за которого хочешь играть: Воитель — warrior, '
@@ -76,17 +120,14 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
-    print('Воитель - warrior, Маг - mage, Лекарь - healer')
-    char_class = choice_char_class()
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-if __name__ == "__main__":
-    main()
